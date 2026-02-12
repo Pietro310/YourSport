a23b52ad105df319c7586ae7ec9679e2ab680e3f
Pietro310 <128912989+Pietro310@users.noreply.github.com>
2026-02-12 17:15:40 +0100

    test commit pietro

--- src/main/java/com/mycompany/yoursport/YourSport.java
+++ src/main/java/com/mycompany/yoursport/YourSport.java
@@ -12,7 +12,5 @@ public class YourSport {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Test 1");
-        System.out.println("Test bisnelo");
     }
 }
