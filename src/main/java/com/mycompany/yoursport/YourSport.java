df74a4daa07a393e8cc21e66599878b827591ae9
david <david@192.168.1.16>
2026-02-12 16:15:26 +0100

    test commit 2

--- src/main/java/com/mycompany/yoursport/YourSport.java
+++ src/main/java/com/mycompany/yoursport/YourSport.java
@@ -13,5 +13,6 @@ public class YourSport {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Test 1");
+        System.out.println("Test 2");
     }
 }
